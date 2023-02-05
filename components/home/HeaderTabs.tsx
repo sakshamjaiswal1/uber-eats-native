ce264f223ed10d3369facb47a0c2c69aec7316a0
sakshamjaiswal1 <sjais973@gmail.com>
2023-02-05 18:40:03 +0530

    initial-routes

---
+++ components/home/HeaderTabs.tsx
@@ -0,0 +1,49 @@
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+
+type Props = {
+  text: string;
+  btnColor: string;
+  textColor: string;
+};
+
+export const HeaderTabs: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <HeaderButton text="Delivery" btnColor="black" textColor="white" />
+      <HeaderButton text="Pickupzz" btnColor="white" textColor="black" />
+    </View>
+  );
+};
+const HeaderButton: React.FC<Props> = ({text, textColor, btnColor}) => {
+  return (
+    <TouchableOpacity
+      style={[
+        {
+          backgroundColor: btnColor,
+        },
+        styles.headerBtnTouch,
+      ]}>
+      <Text style={[{color: textColor}, styles.headerBtnTouch]}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+export default HeaderTabs;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerBtnTouch: {
+    paddingHorizontal: 16,
+    paddingVertical: 6,
+    borderRadius: 30,
+  },
+  headerBtn: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+// =AIzaSyBKwklB8hvRWl1pr2f-YirvbF94brtNmyI
---
+++ screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import HeaderTabs from '../components/home/HeaderTabs';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
