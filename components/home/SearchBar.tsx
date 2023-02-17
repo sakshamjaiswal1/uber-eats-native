19ddac9879129af7c8bba33c29b4aaaf96a7d236
sakshamjaiswal1 <sjais973@gmail.com>
2023-02-17 17:24:33 +0530

    search bar fixes #8

--- components/home/SearchBar.tsx
+++ components/home/SearchBar.tsx
@@ -1,7 +1,8 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import React from 'react';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 type Props = {
   setCity: React.Dispatch<React.SetStateAction<string>>;
 };
@@ -34,6 +35,23 @@ const SearchBar: React.FC<Props> = ({setCity}) => {
             marginRight: 10,
           },
         }}
+        renderLeftButton={() => (
+          <View style={styles.renderLeft}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton={() => {
+          return (
+            <View style={styles.renderRight}>
+              <AntDesign
+                name="clockcircle"
+                size={11}
+                style={{marginRight: 6}}
+              />
+              <Text>Search</Text>
+            </View>
+          );
+        }}
       />
     </View>
   );
@@ -45,6 +63,17 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#000000',
+  },
+  renderLeft: {
+    marginLeft: 15,
+  },
+  renderRight: {
+    flexDirection: 'row',
+    marginRight: 8,
+    backgroundColor: 'white',
+    padding: 9,
+    borderRadius: 30,
+    alignItems: 'center',
   },
 });
 
